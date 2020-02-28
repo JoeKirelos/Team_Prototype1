fa4770ad4d5b1332c4dd4d42aaaee24ac8a91b48
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2020-02-28 17:17:12 -0500

    initial commit for team pt1

---
+++ Assets/moveUpBullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveUpBullet : MonoBehaviour
+{
+    private Rigidbody2D bullet;
+    private Vector2 upwardsMomentum = new Vector2(0f, 1f);
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bullet.AddForce(upwardsMomentum * speed);
+    }
+}
