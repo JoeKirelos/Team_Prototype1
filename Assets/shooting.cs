d09b557448fda4b7f7cab110bc5ba73bc0b24bd4
William L'Eriger <espermentalout@gmail.com>
2020-03-02 00:00:34 -0500

    TP1: Added enemy sprites and shooting sounds

--- Assets/shooting.cs
+++ Assets/shooting.cs
@@ -8,10 +8,13 @@ public class shooting : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float randomizer = 0.1f;
+    public AudioClip shootingSound;
     // Start is called before the first frame update
     void Start()
     {
         Invoke("Shoot", 0);
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = shootingSound;
     }
 
     // Update is called once per frame
@@ -25,5 +28,6 @@ public class shooting : MonoBehaviour
         shootInterval += randomizer;
         Invoke("Shoot", shootInterval);
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GetComponent<AudioSource>().PlayOneShot(shootingSound);
     }
 }
